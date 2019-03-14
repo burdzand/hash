537f34b65ebba25320e50eb551328450d0ae1b72
Anderson Borges <andersondborges@gmail.com>
2019-03-14 18:34:18 -0300

    Set User not Found

--- client/client/app/views.py
+++ client/client/app/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Product
+from .models import Product, User
 from .serializer import ProductSerializer
 
 import logging
@@ -9,9 +9,14 @@ logger = logging.getLogger(__name__)
 
 class ListProducts(APIView):
     def get(self, request):
-        userID = request.GET.get('X-USER-ID', '')
+        userID = request.GET.get('X-USER-ID', None)
         if not userID:
           return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user =  User.objects.get(id=userID)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True, context={'userID': userID})
         return Response(serializer.data)
